80d6acff25ad76304d3526179fadbe51c2dcd282
KelvinP25 <kelvincastillo9911@gmail.com>
2021-11-14 23:49:12 -0300

    Creando la Migracion Inicial, la base de datos y reparando algo en registro de ciudades

---
+++ ARS-System/Migrations/20211115024129_Migracion_Inical.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ARS_System.Migrations
+{
+    public partial class Migracion_Inical : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Ciudades",
+                columns: table => new
+                {
+                    CiudadId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NombreCiudad = table.Column<string>(type: "TEXT", nullable: true),
+                    Provincia = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ciudades", x => x.CiudadId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ciudades");
+        }
+    }
+}
