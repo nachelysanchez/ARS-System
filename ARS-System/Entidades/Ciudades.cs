db3bd6b5cef87c7706f990ce239bc71f9f70c145
KelvinP25 <kelvincastillo9911@gmail.com>
2021-11-14 21:18:42 -0300

    Creando entidad Ciudades

---
+++ ARS-System/Entidades/Ciudades.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ARS_System.Entidades
+{
+    public class Ciudades
+    {
+        [Key]
+        public int CiudadId { get; set; }
+        public string NombreCiudad { get; set; }
+        public string Provincia { get; set; }
+    }
+}
