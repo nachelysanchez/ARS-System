c3538ba1cb053e01fe2bbccc7c11990cfade86e9
KelvinP25 <kelvincastillo9911@gmail.com>
2021-11-14 22:23:59 -0300

    Agregando CiudadesBLL a la BLL

---
+++ ARS-System/BLL/CiudadesBLL.cs
@@ -0,0 +1,172 @@
+﻿using ARS_System.DAL;
+using ARS_System.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARS_System.BLL
+{
+    public class CiudadesBLL
+    {
+        public static bool Guardar(Ciudades ciudades)
+        {
+            if (!Existe(ciudades.CiudadId))
+            {
+                return Insertar(ciudades);
+            }
+            else
+            {
+                return Modificar(ciudades);
+            }
+        }
+
+        private static bool Insertar(Ciudades ciudades)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+
+                contexto.Ciudades.Add(ciudades);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+        public static bool Modificar(Ciudades ciudades)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(ciudades).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static bool Existe(int id)
+        {
+            bool encontrado = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                encontrado = contexto.Ciudades.Any(e => e.CiudadId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+
+        public static Ciudades Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Ciudades ciudades;
+
+            try
+            {
+                ciudades = contexto.Ciudades.Find(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return ciudades;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var ciudades = contexto.Ciudades.Find(id);
+                if(ciudades != null)
+                {
+                    contexto.Ciudades.Remove(ciudades);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+
+        public static List<Ciudades> GetList(Expression<Func<Ciudades, bool>> criterio)
+        {
+            List<Ciudades> lista = new List<Ciudades>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Ciudades.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+        public static List<Ciudades> GetCiudades()
+        {
+            List<Ciudades> lista = new List<Ciudades>();
+            Contexto contexto = new Contexto();
+            try
+            {
+                lista = contexto.Ciudades.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+    }
+}
