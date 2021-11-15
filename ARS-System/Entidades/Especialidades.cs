8d086c00c4147098bf6bf255a6ea92c7f527ec9b
nachelysanchez <nashely-5@hotmail.com>
2021-11-15 15:02:22 -0400

    Programando la entidad Especialidades, y creando la clase Usuarios

---
+++ ARS-System/Entidades/Especialidades.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARS_System.Entidades
+{
+    public class Especialidades
+    {
+        [Key]
+        public int EspecialidadId { get; set; }
+        public string NombreEspecialidad { get; set; }
+    }
+}
