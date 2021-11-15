11019d95b048eff4bbb606e660b10e8895421aab
KelvinP25 <kelvincastillo9911@gmail.com>
2021-11-15 00:10:11 -0300

    Creando codigo del registro de ciudades

--- ARS-System/UI/Registros/rCiudades.xaml.cs
+++ ARS-System/UI/Registros/rCiudades.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ARS_System.BLL;
+using ARS_System.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,29 +21,78 @@ namespace ARS_System.UI.Registros
     /// </summary>
     public partial class rCiudades : Window
     {
+        private Ciudades ciudades = new Ciudades();
         public rCiudades()
         {
             InitializeComponent();
+            this.DataContext = ciudades;
+        }
+        private void Limpiar()
+        {
+            this.ciudades = new Ciudades();
+            this.DataContext = ciudades;
+        }
+        private bool Validar()
+        {
+            bool esValido = true;
+            if(CiudadIdTextBox.Text.Length == 0 || NombreCiudadTextBox.Text.Length == 0 || ProvinciaTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("Ingrese El campo faltante", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            return esValido;
         }
 
         private void BuscarButton_Click(object sender, RoutedEventArgs e)
         {
+            var ciudad = CiudadesBLL.Buscar(Utilidades.ToInt(CiudadIdTextBox.Text));
 
+            if(ciudades != null)
+            {
+                this.ciudades = ciudad;
+            }
+            else
+            {
+                this.ciudades = new Ciudades();
+            }
+            this.DataContext = this.ciudades;
         }
 
         private void NuevoButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Limpiar();
         }
 
         private void GuardarButton_Click(object sender, RoutedEventArgs e)
         {
+            if (!Validar())
+                return;
 
+            var paso = CiudadesBLL.Guardar(ciudades);
+
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("Guardado con exitosa!", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("Guardado Fallida", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
         }
 
         private void EliminarButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (CiudadesBLL.Eliminar(Utilidades.ToInt(CiudadIdTextBox.Text)))
+            {
+                Limpiar();
+                MessageBox.Show("Registro eliminado!", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("No fue posible eliminar", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
