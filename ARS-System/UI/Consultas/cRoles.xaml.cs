5c31cae1b799f9e072a2f1e28b3f7536f8b71b7b
vismarlora <vismar_lora@ucne.edu.do>
2021-11-15 17:39:39 -0400

    Programando la Consulta de la Entidad Roles

--- ARS-System/UI/Consultas/cRoles.xaml.cs
+++ ARS-System/UI/Consultas/cRoles.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ARS_System.BLL;
+using ARS_System.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,7 +28,31 @@ namespace ARS_System.UI.Consultas
 
         private void BuscarButton_Click(object sender, RoutedEventArgs e)
         {
+            var listado = new List<Roles>();
 
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0: //Listado
+                        listado = RolesBLL.GetRoles();
+                        break;
+                    case 1: //Id
+                        listado = RolesBLL.GetList(e => e.RolId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+                    case 2:
+                        listado = RolesBLL.GetList(e => e.Nombre.Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                }
+            }
+            else
+            {
+                listado = RolesBLL.GetList(e => true);
+            }
+
+            DatosDataGrid.ItemsSource = null;
+
+            DatosDataGrid.ItemsSource = listado;
         }
     }
 }
