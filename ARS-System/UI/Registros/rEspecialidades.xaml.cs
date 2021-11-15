c08e51a438f1bcd940ecc57f571ff2eeec870807
nachelysanchez <nashely-5@hotmail.com>
2021-11-15 16:01:03 -0400

    Programando los botones del registro de Especialidades

--- ARS-System/UI/Registros/rEspecialidades.xaml.cs
+++ ARS-System/UI/Registros/rEspecialidades.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ARS_System.BLL;
+using ARS_System.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,29 +21,100 @@ namespace ARS_System.UI.Registros
     /// </summary>
     public partial class rEspecialidades : Window
     {
+        private Especialidades especialidad = new Especialidades();
         public rEspecialidades()
         {
             InitializeComponent();
+            this.DataContext = especialidad;
+        }
+
+        private void Cargar()
+        {
+            this.DataContext = null;
+            this.DataContext = especialidad;
+        }
+
+        private void Limpiar()
+        {
+            this.especialidad = new Especialidades();
+            this.DataContext = especialidad;
+        }
+
+        private bool Validar()
+        {
+            bool esValido = true;
+            if (NombreEspecialidadTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("Debe de completar el campo Nombre de Especialidad", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+                NombreEspecialidadTextBox.Focus();
+            }
+            return esValido;
         }
 
         private void BuscarButton_Click_1(object sender, RoutedEventArgs e)
         {
-
+            var especialidad = EspecialidadesBLL.Buscar(Utilidades.ToInt(EspecialidadIdTextBox.Text));
+            if (especialidad != null)
+            {
+                this.especialidad = especialidad;
+            }
+            else
+            {
+                this.especialidad = new Especialidades();
+                MessageBox.Show("No se ha encontrado la Especialidad", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            this.DataContext = this.especialidad;
         }
 
         private void NuevoButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Limpiar();
         }
 
         private void GuardarButton_Click(object sender, RoutedEventArgs e)
         {
+            if (!Validar())
+            {
+                return;
+            }
+            if (EspecialidadesBLL.ExisteNombre(NombreEspecialidadTextBox.Text))
+            {
+                MessageBox.Show("Ya existe esta especialidad. Ingrese otra", "Informacion",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
 
+            var paso = EspecialidadesBLL.Guardar(this.especialidad);
+
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("La especialidad se guardó correctamente", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("La especialidad no se guardó", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void EliminarButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (EspecialidadesBLL.Eliminar(Utilidades.ToInt(EspecialidadIdTextBox.Text)))
+            {
+                Limpiar();
+                MessageBox.Show("Especialidad eliminada", "Exito",
+                    MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("La especialidad no se logró eliminar", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
     }
 }
