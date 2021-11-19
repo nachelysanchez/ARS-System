2c4e2a481b1d72eda69811473fd6481964561cb1
nachelysanchez <nashely-5@hotmail.com>
2021-11-18 20:25:10 -0400

    Creando la entidad Provincias

---
+++ ARS-System/Entidades/Provincias.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARS_System.Entidades
+{
+    public class Provincias
+    {
+        [Key]
+        public int ProvinciaId { get; set; }
+        public string Nombres { get; set; }
+    }
+}
