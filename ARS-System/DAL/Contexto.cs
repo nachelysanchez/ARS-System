7ad2d39ff9430386563e7e5b908102d279bab747
nachelysanchez <nashely-5@hotmail.com>
2021-11-18 20:43:25 -0400

    Agregando la entidad provincias al Contexto, y creando las provincias en el onModelCreating

--- ARS-System/DAL/Contexto.cs
+++ ARS-System/DAL/Contexto.cs
@@ -12,13 +12,47 @@ namespace ARS_System.DAL
     {
         public DbSet<Ciudades> Ciudades { get; set; }
         public DbSet<Especialidades> Especialidades { get; set; }
-
+        public DbSet<Provincias> Provincias { get; set; }
         public DbSet<Roles> Roles { get; set; }
 
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite(@"Data Source = DATA/ARS-System.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Provincias>().HasData(new Provincias
+            {
+                ProvinciaId = 1,
+                Nombres = "Duarte"
+            });
+            modelBuilder.Entity<Provincias>().HasData(new Provincias
+            {
+                ProvinciaId = 2,
+                Nombres = "María Trinidad Sánchez"
+            });
+            modelBuilder.Entity<Provincias>().HasData(new Provincias
+            {
+                ProvinciaId = 3,
+                Nombres = "Sánchez Ramirez"
+            });
+            modelBuilder.Entity<Provincias>().HasData(new Provincias
+            {
+                ProvinciaId = 4,
+                Nombres = "Hermanas Mirabal"
+            });
+            modelBuilder.Entity<Provincias>().HasData(new Provincias
+            {
+                ProvinciaId = 5,
+                Nombres = "La Vega"
+            });
+            modelBuilder.Entity<Provincias>().HasData(new Provincias
+            {
+                ProvinciaId = 6,
+                Nombres = "Samaná"
+            });
         }
     }
 }
