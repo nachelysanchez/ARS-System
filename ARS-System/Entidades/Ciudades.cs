9b490f14a3d8c217e8afc3cdcf35638360eaaf7d
KelvinP25 <kelvincastillo9911@gmail.com>
2021-11-20 01:04:37 -0300

    Arreglando la entidad ciudades

--- ARS-System/Entidades/Ciudades.cs
+++ ARS-System/Entidades/Ciudades.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +13,10 @@ namespace ARS_System.Entidades
     {
         [Key]
         public int CiudadId { get; set; }
-        public string NombreCiudad { get; set; }
-        public string Provincia { get; set; }
+        public string Nombres { get; set; }
+        public int ProvinciaId { get; set; }
+
+        [ForeignKey("ProvinciaId")]
+        virtual public Provincias Provincias { get; set; }
     }
 }
