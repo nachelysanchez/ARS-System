0a48a5e901cedebb550382fa138c5245fc307c92
KelvinP25 <kelvincastillo9911@gmail.com>
2021-11-20 14:30:53 -0300

    Creando la entidad Permisos

---
+++ ARS-System/Entidades/Permisos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARS_System.Entidades
+{
+    public class Permisos
+    {
+        [Key]
+        public int PermisoId { get; set; }
+        public string Nombre { get; set; }
+    }
+}
