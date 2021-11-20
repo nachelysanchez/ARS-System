43b014df4f0fe5886deda921c721451af1f65f48
KelvinP25 <kelvincastillo9911@gmail.com>
2021-11-20 16:33:38 -0300

    Creando el diseño del registro de Permisos

---
+++ ARS-System/UI/Registros/rPermisos.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ARS_System.UI.Registros
+{
+    /// <summary>
+    /// Interaction logic for rPermisos.xaml
+    /// </summary>
+    public partial class rPermisos : Window
+    {
+        public rPermisos()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
