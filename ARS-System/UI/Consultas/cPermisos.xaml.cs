d13d7e86a303c6f877d892ff5b5ff63a7352f422
KelvinP25 <kelvincastillo9911@gmail.com>
2021-11-20 17:51:23 -0300

    Creando el diseño de la consulta de Permisos

---
+++ ARS-System/UI/Consultas/cPermisos.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ARS_System.UI.Consultas
+{
+    /// <summary>
+    /// Interaction logic for cPermisos.xaml
+    /// </summary>
+    public partial class cPermisos : Window
+    {
+        public cPermisos()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
