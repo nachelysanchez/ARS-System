d4054446be525ae540833f49f3e7c17a9f10d553
KelvinP25 <kelvincastillo9911@gmail.com>
2021-11-20 01:57:31 -0300

    Agrreglando Codigo de consulta de ciudades

---
+++ ars-system/UI/Consultas/cCiudades.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using ARS_System.BLL;
+using ARS_System.Entidades;
+
+namespace ARS_System.UI.Consultas
+{
+    /// <summary>
+    /// Interaction logic for cCiudades.xaml
+    /// </summary>
+    public partial class cCiudades : Window
+    {
+        public cCiudades()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Ciudades>();
+
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        listado = CiudadesBLL.GetCiudades();
+                        break;
+                    case 1:
+                        listado = CiudadesBLL.GetList(e => e.CiudadId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+                    case 2:
+                        listado = CiudadesBLL.GetList(e => e.Nombres.Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                    case 3:
+                        listado = CiudadesBLL.GetList(e => e.Provincias.Nombres.Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                }
+            }
+            else
+            {
+                listado = CiudadesBLL.GetList(e => true);
+            }
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
+        }
+    }
+}
