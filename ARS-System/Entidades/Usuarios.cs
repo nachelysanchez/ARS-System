c461d73bef34aa2c79628d8efd1b2f2772e77a6d
KelvinP25 <kelvincastillo9911@gmail.com>
2021-11-21 12:53:22 -0300

    Creando la entidad Usuarios

--- ARS-System/Entidades/Usuarios.cs
+++ ARS-System/Entidades/Usuarios.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ARS_System.Entidades
 {
-    class Usuarios
+    public class Usuarios
     {
+        public int UsuarioId { get; set; }
+        public string Nombres { get; set; }
+        public string Username { get; set; }
+        public string Contrasena { get; set; }
+        public int RolId { get; set; }
+
+        [ForeignKey("RolId")]
+        public virtual Roles Roles { get; set; }
+
     }
 }
