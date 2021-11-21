91f01c9390710d04e97b1af6547e8118b591b95d
KelvinP25 <kelvincastillo9911@gmail.com>
2021-11-21 18:02:26 -0300

    Creando la BLL del Login

---
+++ ARS-System/BLL/LoginBLL.cs
@@ -0,0 +1,64 @@
+﻿using ARS_System.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARS_System.BLL
+{
+    public class LoginBLL
+    {
+        public static bool Validar(string Username, string Contrasena)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var validar = from usuarios in contexto.Usuarios
+                              where usuarios.Username == Username
+                              && usuarios.Contrasena == GetSHA256(Contrasena)
+                              select usuarios;
+
+                if (validar.Count() > 0)
+                {
+                    paso = true;
+                }
+                else
+                {
+                    paso = false;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static string GetSHA256(string contrasena)
+        {
+            SHA256 sha256 = SHA256Managed.Create();
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder sb = new StringBuilder();
+
+            stream = sha256.ComputeHash(encoding.GetBytes(contrasena));
+
+            for (int i = 0; i < stream.Length; i++)
+            {
+                sb.AppendFormat("{0:x2}", stream[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
