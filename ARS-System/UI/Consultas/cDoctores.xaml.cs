bfbbca176a8ff82c8c361e8924346383d965cfea
vismarlora <vismar_lora@ucne.edu.do>
2021-11-22 17:40:25 -0400

    Creando el codigo de la Consulta de Doctores

--- ARS-System/UI/Consultas/cDoctores.xaml.cs
+++ ARS-System/UI/Consultas/cDoctores.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ARS_System.BLL;
+using ARS_System.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,7 +28,42 @@ namespace ARS_System.UI.Consultas
 
         private void BuscarButton_Click(object sender, RoutedEventArgs e)
         {
+            var listado = new List<Doctores>();
 
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        listado = DoctoresBLL.GetDoctores();
+                        break;
+                    case 1:
+                        listado = DoctoresBLL.GetList(e => e.DoctorId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+                    case 2:
+
+                        listado = DoctoresBLL.GetList(e => e.Nombres.Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                    case 3:
+                        listado = DoctoresBLL.GetList(e => e.Celular.Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                    case 4:
+                        listado = DoctoresBLL.GetList(e => e.Telefono.Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                    case 5:
+                        listado = DoctoresBLL.GetList(e => e.CiudadId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+                    case 6:
+                        listado = DoctoresBLL.GetList(e => e.Exequatur.Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                }
+            }
+            else
+            {
+                listado = DoctoresBLL.GetList(e => true);
+            }
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
         }
     }
 }
