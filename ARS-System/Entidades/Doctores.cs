3b700b6c1f7b1d637165260145add2084df8817b
vismarlora <vismar_lora@ucne.edu.do>
2021-11-22 16:51:27 -0400

    Creando la Entidad Doctores

---
+++ ARS-System/Entidades/Doctores.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARS_System.Entidades
+{
+    public class Doctores
+    {
+        [Key]
+        public int DoctorId { get; set; }
+        public string Nombres { get; set; }
+        public string Celular { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public int CiudadId { get; set; }
+        public string Exequatur { get; set; }
+
+        [ForeignKey("DoctorId")]
+        public virtual List<DoctoresDetalle> Detalle { get; set; } = new List<DoctoresDetalle>();
+
+        [ForeignKey("CiudadId")]
+
+        public Ciudades Ciudades { get; set; }
+    }
+}
