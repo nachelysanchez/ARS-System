b758acdcc4bcd4a53a2aa5c2d5053d99d77a9535
nachelysanchez <nashely-5@hotmail.com>
2021-11-22 18:06:45 -0400

    Programando los metodos Existe, Guardar, Insertar, Modificar, Eliminar y Buscar en DiagnosticosBLL

---
+++ ARS-System/BLL/DiagnosticosBLL.cs
@@ -0,0 +1,129 @@
+﻿using ARS_System.DAL;
+using ARS_System.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARS_System.BLL
+{
+    public class DiagnosticosBLL
+    {
+        public static bool Existe(int id)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+            try
+            {
+                encontrado = contexto.Diagnosticos.Any(e => e.DiagnosticoId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
+        }
+        public static bool Guardar(Diagnosticos diagnostico)
+        {
+            if (!Existe(diagnostico.DiagnosticoId))
+            {
+                return Insertar(diagnostico);
+            }
+            else
+            {
+                return Modificar(diagnostico);
+            }
+        }
+        private static bool Insertar(Diagnosticos diagnosticos)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                contexto.Diagnosticos.Add(diagnosticos);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        public static bool Modificar(Diagnosticos diagnosticos)
+        {
+            Contexto contexto = new Contexto();
+            bool paso = false;
+
+            try
+            {
+                contexto.Entry(diagnosticos).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            try
+            {
+                var diagnostico = contexto.Diagnosticos.Find(id);
+
+                if (diagnostico != null)
+                {
+                    contexto.Diagnosticos.Remove(diagnostico);
+                    paso = contexto.SaveChanges() > 0;
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return paso;
+        }
+        public static Diagnosticos Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Diagnosticos diagnostico;
+
+            try
+            {
+                diagnostico = contexto.Diagnosticos.Find(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return diagnostico;
+        }
+    }
+}
