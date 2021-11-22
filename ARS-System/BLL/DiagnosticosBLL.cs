70eba5dfcfc3444cae5ef241512ee906fc9f35c3
nachelysanchez <nashely-5@hotmail.com>
2021-11-22 18:09:11 -0400

    Programando el metodo GetList, GetDiagnosticos y ExisteNombre en DiagnosticosBLL

--- ARS-System/BLL/DiagnosticosBLL.cs
+++ ARS-System/BLL/DiagnosticosBLL.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -124,6 +125,68 @@ namespace ARS_System.BLL
                 contexto.Dispose();
             }
             return diagnostico;
+        }
+
+        public static List<Diagnosticos> GetList(Expression<Func<Diagnosticos, bool>> criterio)
+        {
+            Contexto contexto = new Contexto();
+            List<Diagnosticos> lista = new List<Diagnosticos>();
+
+            try
+            {
+                lista = contexto.Diagnosticos.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+
+        public static List<Diagnosticos> GetDiagnosticos()
+        {
+            Contexto contexto = new Contexto();
+            List<Diagnosticos> lista = new List<Diagnosticos>();
+
+            try
+            {
+                lista = contexto.Diagnosticos.ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return lista;
+        }
+        public static bool ExisteNombre(string nombre)
+        {
+            Contexto contexto = new Contexto();
+            bool encontrado = false;
+
+            try
+            {
+                encontrado = contexto.Diagnosticos.Any(e => e.Nombres == nombre);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return encontrado;
         }
     }
 }
