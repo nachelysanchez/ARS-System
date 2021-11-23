328e3eda26c5596649ea8b8d37c6c812feea503d
nachelysanchez <nashely-5@hotmail.com>
2021-11-22 22:31:50 -0400

    Programando la consulta de prestadores

--- ARS-System/UI/Consultas/cPrestadores.xaml.cs
+++ ARS-System/UI/Consultas/cPrestadores.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ARS_System.BLL;
+using ARS_System.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,7 +28,42 @@ namespace ARS_System.UI.Consultas
 
         private void BuscarButton_Click(object sender, RoutedEventArgs e)
         {
+            var listado = new List<Prestadores>();
 
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0: //Listado
+                        listado = PrestadoresBLL.GetPrestadores();
+                        break;
+                    case 1: //Id
+                        listado = PrestadoresBLL.GetList(e => e.PrestadorId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+                    case 2: //Nombre
+                        listado = PrestadoresBLL.GetList(e => e.Nombres.Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                    case 3: //RNC
+                        listado = PrestadoresBLL.GetList(e => e.RNC.Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                    case 4: //Direccion
+                        listado = PrestadoresBLL.GetList(e => e.Direccion.Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                    case 5: //Ciudad
+                        listado = PrestadoresBLL.GetList(e => e.Ciudad.Nombres.Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                    case 6: //Telefono
+                        listado = PrestadoresBLL.GetList(e => e.Telefono.Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                }
+            }
+            else
+            {
+                listado = PrestadoresBLL.GetList(e => true);
+            }
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
         }
     }
 }
