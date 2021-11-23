e8dce15803b3b32c84a427fc50de4785de6933c0
nachelysanchez <nashely-5@hotmail.com>
2021-11-22 23:46:36 -0400

    Creando entidad Sexos

---
+++ ARS-System/Entidades/Sexos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARS_System.Entidades
+{
+    public class Sexos
+    {
+        [Key]
+        public int SexoId { get; set; }
+        public string Nombres { get; set; }
+    }
+}
