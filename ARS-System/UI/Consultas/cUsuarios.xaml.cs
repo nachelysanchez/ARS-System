251650795f388535314f287693049a41b451e7fe
KelvinP25 <kelvincastillo9911@gmail.com>
2021-11-25 23:32:14 -0300

    Creando codigo de la consulta de usuarios

--- ARS-System/UI/Consultas/cUsuarios.xaml.cs
+++ ARS-System/UI/Consultas/cUsuarios.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ARS_System.BLL;
+using ARS_System.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +21,37 @@ namespace ARS_System.UI.Consultas
     /// </summary>
     public partial class cUsuarios : Window
     {
+        public Roles rol = new Roles();
         public cUsuarios()
         {
             InitializeComponent();
+        }
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+            var listado = new List<Usuarios>();
+
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        listado = UsuariosBLL.GetList(e => e.UsuarioId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+                    case 1:
+                        listado = UsuariosBLL.GetList(e => e.Nombres.Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                    case 2:
+                        listado = UsuariosBLL.GetList(e => e.Username.Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                   
+                }
+            }
+            else
+            {
+                listado = UsuariosBLL.GetList(e => true);
+            }
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
         }
     }
 }
