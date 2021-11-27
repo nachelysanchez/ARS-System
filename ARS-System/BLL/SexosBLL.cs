ff3afcb69f7aec752a80cde4dfa34e092901f99e
nachelysanchez <nashely-5@hotmail.com>
2021-11-27 19:14:25 -0400

    Programando el método Buscar en SexosBLL

---
+++ ARS-System/BLL/SexosBLL.cs
@@ -0,0 +1,34 @@
+﻿using ARS_System.DAL;
+using ARS_System.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARS_System.BLL
+{
+    public class SexosBLL
+    {
+        public static Sexos Buscar(int id)
+        {
+            Sexos sexo;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                sexo = contexto.Sexos.Find(id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return sexo;
+        }
+    }
+}
