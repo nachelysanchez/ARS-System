c80a400e48541f38ecf2a1fc7ec1371cb37be47e
nachelysanchez <nashely-5@hotmail.com>
2021-11-27 20:27:19 -0400

    Programando el GetList en el SexosBLL

--- ARS-System/BLL/SexosBLL.cs
+++ ARS-System/BLL/SexosBLL.cs
@@ -3,6 +3,7 @@ using ARS_System.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -29,6 +30,46 @@ namespace ARS_System.BLL
                 contexto.Dispose();
             }
             return sexo;
+        }
+
+        public static List<Sexos> GetList(Expression<Func<Sexos, bool>> criterio)
+        {
+            List<Sexos> Lista = new List<Sexos>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                Lista = contexto.Sexos.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return Lista;
+        }
+
+        public static List<Sexos> GetSexos()
+        {
+            List<Sexos> Lista = new List<Sexos>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                Lista = contexto.Sexos.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+            return Lista;
         }
     }
 }
