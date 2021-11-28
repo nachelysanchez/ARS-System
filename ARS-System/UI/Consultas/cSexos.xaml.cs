9bba69152552c21d527a7fac40d12c8cec213742
nachelysanchez <nashely-5@hotmail.com>
2021-11-27 21:10:58 -0400

    Programando la consulta de Sexos

--- ARS-System/UI/Consultas/cSexos.xaml.cs
+++ ARS-System/UI/Consultas/cSexos.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ARS_System.BLL;
+using ARS_System.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,7 +28,29 @@ namespace ARS_System.UI.Consultas
 
         private void BuscarButton_Click_3(object sender, RoutedEventArgs e)
         {
+            var listado = new List<Sexos>();
 
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0:
+                        listado = SexosBLL.GetSexos();
+                        break;
+                    case 1:
+                        listado = SexosBLL.GetList(e => e.SexoId == Utilidades.ToInt(CriterioTextBox.Text));
+                        break;
+                    case 2:
+                        listado = SexosBLL.GetList(e => e.Nombres.Contains(CriterioTextBox.Text.ToLower()));
+                        break;
+                }
+            }
+            else
+            {
+                listado = SexosBLL.GetList(e => true);
+            }
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
         }
     }
 }
