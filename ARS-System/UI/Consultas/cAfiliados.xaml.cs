89ed858b2deaf653f64c81ded36c4425ef3a318e
nachelysanchez <nashely-5@hotmail.com>
2021-11-28 18:04:40 -0400

    Arreglando Bindings en la consulta de afiliados

--- ARS-System/UI/Consultas/cAfiliados.xaml.cs
+++ ARS-System/UI/Consultas/cAfiliados.xaml.cs
@@ -25,7 +25,101 @@ namespace ARS_System.UI.Consultas
         }
         private void BuscarButton_Click_2(object sender, RoutedEventArgs e)
         {
+            var listado = new List<Afiliados>();
 
+            if (CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    case 0: //Id
+                        if (DesdeDatePicker.SelectedDate != null && HastaDatePicker != null)
+                        {
+                            listado = ProyectosBLL.GetList(x => x.ProyectoId == Utilidades.ToInt(CriterioTextBox.Text)
+                            && x.Fecha.Date <= HastaDatePicker.SelectedDate && x.Fecha.Date >= DesdeDatePicker.SelectedDate);
+                        }
+                        else if (HastaDatePicker.SelectedDate != null)
+                        {
+                            listado = ProyectosBLL.GetList(x => x.ProyectoId == Utilidades.ToInt(CriterioTextBox.Text)
+                            && x.Fecha.Date <= HastaDatePicker.SelectedDate);
+                        }
+                        else if (DesdeDatePicker.SelectedDate != null)
+                        {
+                            listado = ProyectosBLL.GetList(x => x.ProyectoId == Utilidades.ToInt(CriterioTextBox.Text)
+                            && x.Fecha.Date >= DesdeDatePicker.SelectedDate);
+                        }
+                        else
+                        {
+                            listado = ProyectosBLL.GetList(e => e.ProyectoId == Utilidades.ToInt(CriterioTextBox.Text));
+                        }
+                        break;
+                    case 1: //Descripcion
+                        if (DesdeDatePicker.SelectedDate != null && HastaDatePicker != null)
+                        {
+                            listado = ProyectosBLL.GetList(x => x.DescripcionProyecto.Contains(CriterioTextBox.Text.ToLower())
+                            && x.Fecha.Date <= HastaDatePicker.SelectedDate && x.Fecha.Date >= DesdeDatePicker.SelectedDate);
+                        }
+                        else if (HastaDatePicker.SelectedDate != null)
+                        {
+                            listado = ProyectosBLL.GetList(x => x.DescripcionProyecto.Contains(CriterioTextBox.Text.ToLower())
+                            && x.Fecha.Date <= HastaDatePicker.SelectedDate);
+                        }
+                        else if (DesdeDatePicker.SelectedDate != null)
+                        {
+                            listado = ProyectosBLL.GetList(x => x.DescripcionProyecto.Contains(CriterioTextBox.Text.ToLower())
+                            && x.Fecha.Date >= DesdeDatePicker.SelectedDate);
+                        }
+                        else
+                        {
+                            listado = ProyectosBLL.GetList(x => x.DescripcionProyecto.Contains(CriterioTextBox.Text.ToLower()));
+                        }
+                        break;
+                    case 2: //Total de Minutos
+                        if (DesdeDatePicker.SelectedDate != null && HastaDatePicker != null)
+                        {
+                            listado = ProyectosBLL.GetList(x => x.Total == Utilidades.ToInt(CriterioTextBox.Text)
+                            && x.Fecha.Date <= HastaDatePicker.SelectedDate && x.Fecha.Date >= DesdeDatePicker.SelectedDate);
+                        }
+                        else if (HastaDatePicker.SelectedDate != null)
+                        {
+                            listado = ProyectosBLL.GetList(x => x.Total == Utilidades.ToInt(CriterioTextBox.Text)
+                            && x.Fecha.Date <= HastaDatePicker.SelectedDate);
+                        }
+                        else if (DesdeDatePicker.SelectedDate != null)
+                        {
+                            listado = ProyectosBLL.GetList(x => x.Total == Utilidades.ToInt(CriterioTextBox.Text)
+                            && x.Fecha.Date >= DesdeDatePicker.SelectedDate);
+                        }
+                        else
+                        {
+                            listado = ProyectosBLL.GetList(x => x.Total == Utilidades.ToInt(CriterioTextBox.Text));
+                        }
+                        break;
+                }
+            }
+            else
+            {
+                listado = ProyectosBLL.GetList(e => true);
+            }
+
+            if (DesdeDatePicker.SelectedDate != null && HastaDatePicker.SelectedDate != null && FiltroComboBox.SelectedIndex < 0)
+            {
+                listado = ProyectosBLL.GetList(x => x.Fecha.Date >= DesdeDatePicker.SelectedDate &&
+                x.Fecha.Date <= HastaDatePicker.SelectedDate);
+            }
+
+            if (HastaDatePicker.SelectedDate != null && FiltroComboBox.SelectedIndex < 0)
+            {
+                listado = ProyectosBLL.GetList(x => x.Fecha.Date >= DesdeDatePicker.SelectedDate &&
+                x.Fecha.Date <= HastaDatePicker.SelectedDate);
+            }
+
+            if (DesdeDatePicker.SelectedDate != null && FiltroComboBox.SelectedIndex < 0)
+            {
+                listado = ProyectosBLL.GetList(x => x.Fecha.Date >= DesdeDatePicker.SelectedDate);
+            }
+
+            DatosDataGrid.ItemsSource = null;
+            DatosDataGrid.ItemsSource = listado;
         }
     }
 }
