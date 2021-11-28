faa4d26243239cff0156bc50b6babf5b2a12b28e
nachelysanchez <nashely-5@hotmail.com>
2021-11-27 22:41:01 -0400

    Diseñando el registro de Afiliados

---
+++ ARS-System/UI/Registros/rAfiliados.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ARS_System.UI.Registros
+{
+    /// <summary>
+    /// Interaction logic for rAfiliados.xaml
+    /// </summary>
+    public partial class rAfiliados : Window
+    {
+        public rAfiliados()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BuscarButton_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AgregarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Agregar1Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Agregar2Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
