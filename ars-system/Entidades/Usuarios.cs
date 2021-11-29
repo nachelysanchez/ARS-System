8ee34d961e21931f5c513eb7644fe3a474659cb8
KelvinP25 <kelvincastillo9911@gmail.com>
2021-11-29 10:35:57 -0300

    Agregando un campo a la entidad usuarios

---
+++ ars-system/Entidades/Usuarios.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARS_System.Entidades
+{
+    public class Usuarios
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public string Nombres { get; set; }
+        public string Username { get; set; }
+        public string Contrasena { get; set; }
+        public int RolId { get; set; }
+        public int TotalAsignado { get; set; }
+
+        [ForeignKey("RolId")]
+        public Roles Roles { get; set; }
+
+        [ForeignKey("UsuarioId")]
+        public virtual List<UsuariosDetalle> DetalleUsuario { get; set; } = new List<UsuariosDetalle>();
+        public Usuarios()
+        {
+            UsuarioId = 0;
+            Nombres = "";
+            Username = "";
+            Contrasena = "";
+            RolId = 0;
+        }
+        public Usuarios(int usuarioId, string nombres, string username, string contrasena)
+        {
+            UsuarioId = usuarioId;
+            Nombres = nombres;
+            Username = username;
+            Contrasena = contrasena;
+        }
+
+    }
+}
