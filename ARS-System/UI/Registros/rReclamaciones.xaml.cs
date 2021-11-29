b56f7697e604794286139d7f2bb1a44ead8a32bc
nachelysanchez <nashely-5@hotmail.com>
2021-11-28 23:25:24 -0400

    Diseñando el registro de Reclamaciones

---
+++ ARS-System/UI/Registros/rReclamaciones.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ARS_System.UI.Registros
+{
+    /// <summary>
+    /// Interaction logic for rReclamaciones.xaml
+    /// </summary>
+    public partial class rReclamaciones : Window
+    {
+        public rReclamaciones()
+        {
+            InitializeComponent();
+        }
+
+        private void BuscarButton_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NuevoButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GuardarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EliminarButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AgregarFilaButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RemoverFilaButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
